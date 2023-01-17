b0ab62ca4ed74266895365091ebf20a17fa5f4e9
kipngeno kevin <kipngenokevin254@gmail.com>
2023-01-17 15:54:03 +0300

    Nav bar complete and responsive

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import Home from '../src/pages/Home/Home'
+import Products from '../src/pages/Products/Products'
+import Product from '../src/pages/Product/Product'
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import './app.scss' 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+
+
+
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      {/*<Outlet/>
+      <Footer/>*/}
+    </div>
+  )
+}
+
+const router = createBrowserRouter ([
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/products/:id',
+        element: <Products/>
+      },
+      {
+        path: '/product/:id',
+        element: <Product/>
+      },
+    ]
+  }
+])
+
 function App() {
   return (
     <div>
-      Hello world!
+       <RouterProvider router={router} />
     </div>
   );
 }
