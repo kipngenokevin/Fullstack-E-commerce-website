5a33d35f8f2a37b4afcd23d6d024156c4740fd46
kipngeno kevin <kipngenokevin254@gmail.com>
2023-01-25 15:22:38 +0300

    Unnecessary updates removed

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Import Swiper styles
+
+const Slider = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Slider
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Slider from '../../components/Slider/Slider'
+
 
 const Home = () => {
   return (
-    <div></div>
+    <div>
+      <Slider/>
+    </div>
   )
 }
 
